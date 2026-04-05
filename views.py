e3ade6a92fb20a947e0df5e7094e1aeed04c1c50
pulex <artemkapulnikov@yandex.ru>
2026-04-05 13:28:17 +0500

    colors

--- views.py
+++ views.py
@@ -1,14 +1,15 @@
 import pygame
 
+
 class Colors:
-  TOWER_FRONT_V1 = (128, 128, 128)
-  TOWER_HEADER_V1 = (0, 0, 0)
-  GOBLINS = (0, 255, 0)
-  FIELD = (20, 255, 44)
-  TOWER_FRONT_V2 = (160, 128 ,160)
-  TOWER_HEADER_V2 = (32, 0, 32)
-  TOWER_FRONT_V3 = (192, 128, 192)
-  TOWER_HEADER_V3 = (64, 0, 64)
-  OGRES = (0, 255, 60)
-  BIG_BOB = (255, 62, 255)
+    TOWER_FRONT_V1 = (128, 128, 128)
+    TOWER_HEADER_V1 = (0, 0, 0)
+    GOBLINS = (0, 255, 0)
+    FIELD = (20, 255, 44)
+    TOWER_FRONT_V2 = (160, 128, 160)
+    TOWER_HEADER_V2 = (32, 0, 32)
+    TOWER_FRONT_V3 = (192, 128, 192)
+    TOWER_HEADER_V3 = (64, 0, 64)
+    OGRES = (0, 255, 60)
+    BIG_BOB = (255, 62, 255)
 
